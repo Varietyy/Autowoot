b8ceca07a8b1f7fc3a4844aa26ba0b7935b4af21
Varietyy <chasedss@gmail.com>
2014-12-17 19:32:18 -0500

    Update Autowoot.js

--- Autowoot.js
+++ Autowoot.js
@@ -1,20 +1,8 @@
-API.chatLog("Variety's Autowoot! Commands: /on and /off", true);
-API.on(API.CHAT_COMMAND, command);
-API.on(API.DJ_ADVANCE, autowoot);
-$('#woot').click();
-
-function autowoot() {
-  $('#woot').click();
-}
-
-function command(value) {
-    if (value == "/on") {
-        API.on(API.DJ_ADVANCE, autowoot);
-        $('#woot').click();
-        API.chatLog("Autowoot [ON]", alert);
-    }
-    else if (value == "/off") {
-        API.off(API.DJ_ADVANCE, autowoot);
-        API.chatLog("Autowoot [OFF]", alert);
-    }
+API.on(API.ADVANCE, djAdvance);
+API.chatLog('Variety\'s Autowoot');
+ 
+function djAdvance(obj) {
+    setTimeout(function() {
+        $('#woot').click()
+    },500)
 }
